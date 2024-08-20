d0889f460cab4247c326bce549c7d276db907903
Manuel <manuel@mods.cl>
2024-08-19 23:34:13 -0400

    feat: add synopsis and bad request if data is empty

--- src/main/java/com/example/demo/Movie.java
+++ src/main/java/com/example/demo/Movie.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 public class Movie {
-    // the movie class have name, title, category, and year
     @JsonProperty("id")
     private int id;
 
@@ -19,13 +19,20 @@ public class Movie {
     @JsonProperty("year")
     private int year;
 
+    @JsonProperty("synopsis")
+    private String synopsis;
+
+    @JsonPropertyOrder({ "id", "name", "title", "category", "year", "synopsis" })
+
     // the constructor of the class
-    public Movie(int id, String name, String title, String category, int year) {
+    public Movie(int id, String name, String title, String category, int year, String synopsis) {
         this.id = id;
         this.name = name;
         this.title = title;
         this.category = category;
         this.year = year;
+        this.synopsis = synopsis;
+
     }
 
     // the getter of the id
@@ -61,6 +68,31 @@ public class Movie {
     // the getter of the category
     public String getCategory() {
         return category;
+    }
+
+    // the setter of the category
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // the getter of the year
+    public int getYear() {
+        return year;
+    }
+
+    // the setter of the year
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // the setter of the synopsis
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    // the getter of the synopsis
+    public String getSynopsis() {
+        return synopsis;
     }
 
 }
--- src/main/java/com/example/demo/MoviesController.java
+++ src/main/java/com/example/demo/MoviesController.java
@@ -14,9 +14,15 @@ public class MoviesController {
     ArrayList<Movie> movies = new ArrayList<Movie>();
 
     MoviesController() {
-        movies.add(new Movie(1, "The Godfather", "Francis Ford Coppola", "Crime", 1972));
-        movies.add(new Movie(2, "The Shawshank Redemption", "Frank Darabont", "Drama", 1994));
-        movies.add(new Movie(3, "Pokemon", "Ash Ketchup", "Anime", 2023));
+        movies.add(new Movie(1, "The Godfather", "Francis Ford Coppola", "Crime", 1972,
+                "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."));
+        movies.add(new Movie(2, "The Shawshank Redemption", "Frank Darabont", "Drama", 1994, "Two imprisoned"));
+        movies.add(new Movie(3, "Pokemon", "Ash Ketchup", "Anime", 2023, "Gotta catch them all"));
+        movies.add(new Movie(4, "The Dark Knight", "Christopher Nolan", "Action", 2008,
+                "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept"));
+        movies.add(new Movie(5, "The Lord of the Rings: The Return of the King", "Peter Jackson", "Fantasy", 2003,
+                "Gandalf and Aragorn lead the World"));
+
     }
 
     @GetMapping("/movies")
@@ -28,7 +34,12 @@ public class MoviesController {
     // add a new movie to the list
     @PostMapping("/movies")
     public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
-        // request body json object
+        // validate if all fields are filled
+        if (movie.getId() == 0 || movie.getName() == null || movie.getTitle() == null || movie.getCategory() == null
+                || movie.getYear() == 0 || movie.getSynopsis() == null) {
+
+            return ResponseEntity.badRequest().build();
+        }
 
         movies.add(movie);
         return ResponseEntity.ok(movie);
